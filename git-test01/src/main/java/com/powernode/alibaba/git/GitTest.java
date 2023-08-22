eafed0bd97890ad91c8ac1878326654ab21d6a51
Ywenn <cshn2215393@qq.com>
2023-08-22 09:42:11 +0800

    first commit

---
+++ git-test01/src/main/java/com/powernode/alibaba/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.powernode.alibaba.git;
+
+/**
+ * @version 1.0
+ * @Author Ywenn
+ * @Date 2023/8/22 9:29
+ * @注释
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello! git");
+    }
+}
